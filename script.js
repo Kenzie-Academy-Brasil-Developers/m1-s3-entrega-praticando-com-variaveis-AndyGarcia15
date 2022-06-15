0ef0df2cb71bf6738f9acba4c8e94faa55b49a63
Andy Garcia| <andy_gar@yahoo.com>
2022-06-15 16:40:16 -0300

    entrega finalizada

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = 'Andy';
+let sobrenome = ' Garcia';
+let anoNascimento = 1998;
+let cidade = ' Belo Horizonte';
+let anoAtual = 2022;
+
+console.log(
+    `Eu sou ${nome + sobrenome}, nasci em ${anoNascimento } e tenho ${anoAtual -anoNascimento }, moro em ${cidade}.`
+)
